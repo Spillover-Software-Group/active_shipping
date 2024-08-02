9ffaa67dcb2db80ff6ccc4be23b3ad2253fd7e6e
Gustavo Camello <gucamello@gmail.com>
2024-08-02 08:45:53 +0100

    wip

--- lib/active_shipping/carriers/usps_rest.rb
+++ lib/active_shipping/carriers/usps_rest.rb
@@ -1,7 +1,101 @@
 module ActiveShipping
   class USPSRest < Carrier
-    def find_rates
-      raise "from here"
+    EventDetails = Struct.new(:description, :time, :zoneless_time, :location, :event_code)
+    ONLY_PREFIX_EVENTS = ['DELIVERED','OUT FOR DELIVERY']
+    self.retry_safe = true
+    self.ssl_version = :TLSv1_2
+
+    cattr_reader :name
+    @@name = "USPS"
+
+    LIVE_DOMAIN = 'production.shippingapis.com'
+    LIVE_RESOURCE = 'ShippingAPI.dll'
+
+    TEST_DOMAINS = { # indexed by security; e.g. TEST_DOMAINS[USE_SSL[:rates]]
+      true => 'secure.shippingapis.com',
+      false => 'stg-production.shippingapis.com'
+    }
+
+    MAIL_TYPES = {
+      :package => 'Package',
+      :postcard => 'Postcards or aerogrammes',
+      :matter_for_the_blind => 'Matter for the blind',
+      :envelope => 'Envelope'
+    }
+
+    PACKAGE_PROPERTIES = {
+      'ZipOrigination' => :origin_zip,
+      'ZipDestination' => :destination_zip,
+      'Pounds' => :pounds,
+      'Ounces' => :ounces,
+      'Container' => :container,
+      'Size' => :size,
+      'Machinable' => :machinable,
+      'Zone' => :zone,
+      'Postage' => :postage,
+      'Restrictions' => :restrictions
+    }
+    POSTAGE_PROPERTIES = {
+      'MailService' => :service,
+      'Rate' => :rate
+    }
+
+    US_SERVICES = {
+      :first_class => 'FIRST CLASS',
+      :priority => 'PRIORITY',
+      :express => 'EXPRESS',
+      :bpm => 'BPM',
+      :parcel => 'PARCEL',
+      :media => 'MEDIA',
+      :library => 'LIBRARY',
+      :online => 'ONLINE',
+      :plus => 'PLUS',
+      :all => 'ALL'
+    }
+
+    # Array of U.S. possessions according to USPS: https://www.usps.com/ship/official-abbreviations.htm
+    US_POSSESSIONS = %w(AS FM GU MH MP PW PR VI)
+
+    # Country names:
+    # http://pe.usps.gov/text/Imm/immctry.htm
+    COUNTRY_NAME_CONVERSIONS = {
+      "BA" => "Bosnia-Herzegovina",
+      "CD" => "Congo, Democratic Republic of the",
+      "CG" => "Congo (Brazzaville),Republic of the",
+      "CI" => "Côte d'Ivoire (Ivory Coast)",
+      "CK" => "Cook Islands (New Zealand)",
+      "FK" => "Falkland Islands",
+      "GB" => "Great Britain and Northern Ireland",
+      "GE" => "Georgia, Republic of",
+      "IR" => "Iran",
+      "KN" => "Saint Kitts (St. Christopher and Nevis)",
+      "KP" => "North Korea (Korea, Democratic People's Republic of)",
+      "KR" => "South Korea (Korea, Republic of)",
+      "LA" => "Laos",
+      "LY" => "Libya",
+      "MC" => "Monaco (France)",
+      "MD" => "Moldova",
+      "MK" => "Macedonia, Republic of",
+      "MM" => "Burma",
+      "PN" => "Pitcairn Island",
+      "RU" => "Russia",
+      "SK" => "Slovak Republic",
+      "TK" => "Tokelau (Union) Group (Western Samoa)",
+      "TW" => "Taiwan",
+      "TZ" => "Tanzania",
+      "VA" => "Vatican City",
+      "VG" => "British Virgin Islands",
+      "VN" => "Vietnam",
+      "WF" => "Wallis and Futuna Islands",
+      "WS" => "Western Samoa"
+    }
+
+    def requirements
+      [:client_id, :client_secret, :access_token]
+    end
+
+    def find_rates(origin, destination, packages, options = {})
+      raise "from here origin: #{origin} and destination: #{destination} and packages: #{packages}"
     end
   end
 end
