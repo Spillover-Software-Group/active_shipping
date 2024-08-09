b9a270cd06491fe2104e72d8b27ce7fc9b72a979
Gustavo Camello <gucamello@gmail.com>
2024-08-09 15:23:19 +0100

    wip

--- lib/active_shipping/carriers/usps_rest.rb
+++ lib/active_shipping/carriers/usps_rest.rb
@@ -169,6 +169,9 @@ module ActiveShipping
             new_token_response = ssl_post(
               "#{test ? TEST_URL : LIVE_URL}/oauth2/v3/token",
               params.to_json,
+              headers: {
+                "Content-type" => "application/json"
+              }
             )
 
             raise "resp #{new_token_response}".inspect
