0c8a6d394364993fc9188d325e1102411533c149
Gustavo Camello <gucamello@gmail.com>
2024-08-09 13:09:21 +0100

    wip

--- lib/active_shipping/carriers/usps_rest.rb
+++ lib/active_shipping/carriers/usps_rest.rb
@@ -149,7 +149,7 @@ module ActiveShipping
         "Content-type" => "application/json"
       }
 
-      raise "FROM THSSSS #{full_url, body}".inspect
+      raise "FROM THSSSS #{full_url}".inspect
       response = ssl_post(full_url, body, headers)
 
       raise "the response #{response}".inspect
