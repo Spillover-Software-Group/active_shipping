e71d95b984917013cc6f23b662f404688cb197ef
Gustavo Camello <gucamello@gmail.com>
2024-08-19 14:48:12 +0100

    wip

---
+++ lib/active_shipping/carriers/usps_shipstation.rb
@@ -0,0 +1,82 @@
+module ActiveShipping
+  class USPSShipstation < Carrier
+
+    cattr_reader :name
+    @@name = "USPS Shipstation"
+
+    LIVE_URL = "https://ssapi.shipstation.com"
+
+    def requirements
+      [:api_key, :api_secret]
+    end
+
+    def find_rates(origin, destination, packages, options = {})
+      options = @options.merge(options)
+
+      origin = Location.from(origin)
+      destination = Location.from(destination)
+      packages = Array(packages)
+      
+      success = true
+      message = ''
+      packages_rates = []
+
+      packages.each_with_index do |package, index|
+        begin
+          body = {
+            carrier_code: "stamps_com",
+            fromPostalCode: origin.zip,
+            toState: origin.state,
+            toCountry: origin.country,
+            toPostalCode: destination.zip,
+            weight: {
+              value: package.oz.to_f,
+              :units: "ounces"
+            },
+            dimentions:
+            length: package.inches(:length).to_f,
+            width: package.inches(:width).to_f,
+            height: package.inches(:height).to_f,
+          }
+    
+          request = http_request(
+            "#{LIVE_URL}/shipments/getrates",
+            body.to_json,
+          )
+
+          response = JSON.parse(request)
+
+          package = {
+            package: index,
+            rates: generate_package_rates(response)
+          }
+         
+          packages_rates << package
+        rescue StandardError => e
+          # If for any reason the request fails, we return an error and display the message
+          # "We are unable to calculate shipping rates for the selected items" to the user
+          packages_rates = []
+          break
+        end
+      end
+
+      private
+
+      def http_request(full_url, body, test = false)
+        headers = {
+          "Authorization" => "Basic #{credentials_base64}",
+          "Content-type" => "application/json"
+        }
+
+        request = ssl_post(full_url, body, headers)
+        request
+      end
+
+      def credentials_base64
+        credentials = "#{options[:api_key]}:#{options[:api_secret]}"
+        Base64.strict_encode64(credentials)
+      end
+
+    end
+  end
+end
