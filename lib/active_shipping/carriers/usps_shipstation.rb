52f9b61624c8ba8142352b56dc2082f19dc3637f
Gustavo Camello <gucamello@gmail.com>
2024-08-20 16:38:46 +0100

    wip

--- lib/active_shipping/carriers/usps_shipstation.rb
+++ lib/active_shipping/carriers/usps_shipstation.rb
@@ -1,5 +1,5 @@
 module ActiveShipping
-  class USPSShipstation < Carriers::Shipstation
+  class USPSShipstation < ActiveShipping::Shipstation
     cattr_reader :name
     @@name = "USPS Shipstation"
 
