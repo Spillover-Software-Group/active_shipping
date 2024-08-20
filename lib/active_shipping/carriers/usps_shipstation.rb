98d0c514d2586075323b540762e1ab208b5cb5be
Gustavo Camello <gucamello@gmail.com>
2024-08-20 11:22:27 +0100

    wip

--- lib/active_shipping/carriers/usps_shipstation.rb
+++ lib/active_shipping/carriers/usps_shipstation.rb
@@ -6,6 +6,30 @@ module ActiveShipping
 
     LIVE_URL = "https://ssapi.shipstation.com"
 
+    SERVICE_MAIL_CLASSES = {
+      "USPS First Class Mail - Letter": "usps_first_class_mail_letter",
+      "USPS First Class Mail - Large Envelope or Flat": "usps_first_class_mail_large_envelope",
+      "USPS First Class Mail - Package": "usps_first_class_mail_package",
+
+      "USPS Priority Mail - Package": "usps_first_class_mail_package",
+      "USPS Priority Mail - Medium Flat Rate Box": "usps_first_class_mail_medium_flat",
+      "USPS Priority Mail - Small Flat Rate Box": "usps_first_class_mail_small_flat",
+      "USPS Priority Mail - Large Flat Rate Box": "usps_first_class_mail_large_flat",
+      "USPS Priority Mail - Flat Rate Envelope": "usps_first_class_mail_envolepe_flat",
+      "USPS Priority Mail - Flat Rate Padded Envelope": "usps_first_class_mail_padded_envelope",
+      "USPS Priority Mail - Legal Flat Rate Envelope": "usps_first_class_mail_legal_flat",
+
+
+      "USPS Priority Mail Express - Package": "usps_first_class_mail_package",
+      "USPS Priority Mail Express - Flat Rate Envelope": "usps_first_class_mail_flat_envelope",
+      "USPS Priority Mail Express - Flat Rate Padded Envelope": "usps_first_class_mail_padded_envelope",
+      "USPS Priority Mail Express - Legal Flat Rate Envelope": "usps_first_class_mail_legal_envelope",
+
+      "USPS Media Mail - Package": "usps_media_mail",
+      "USPS Parcel Select Ground - Package": "usps_parcel_select",
+      "USPS Ground Advantage - Package": "usps_ground_advantage",
+    }
+
     def requirements
       [:api_key, :api_secret]
     end
@@ -22,7 +46,7 @@ module ActiveShipping
       packages_rates = []
 
       packages.each_with_index do |package, index|
-        # begin
+        begin
           body = {
             carrierCode: "stamps_com",
             fromPostalCode: origin.zip,
@@ -47,8 +71,6 @@ module ActiveShipping
             body.to_json,
           )
 
-          raise "the request #{request}".inspect
-
           response = JSON.parse(request)
 
           package = {
@@ -57,16 +79,29 @@ module ActiveShipping
           }
          
           packages_rates << package
-        # rescue StandardError => e
-        #   # If for any reason the request fails, we return an error and display the message
-        #   # "We are unable to calculate shipping rates for the selected items" to the user
-        #   packages_rates = []
-        #   break
-        # end
+        rescue StandardError => e
+          # If for any reason the request fails, we return an error and display the message
+          # "We are unable to calculate shipping rates for the selected items" to the user
+          packages_rates = []
+          break
+        end
       end
+
+      raise "THE RESULT FROM HERE #{package_rates}".inspect
     end
 
     private
+
+    def generate_package_rates(response)
+      service_rates = response.map do |service_type|
+        {
+          mail_class: SERVICE_MAIL_CLASSES[":#{service_type["serviceName"]}"],
+          price: service_type["shipmentCost"]
+        }
+      end
+
+      services_rates.compact!
+    end
 
     def http_request(full_url, body, test = false)
       headers = {
