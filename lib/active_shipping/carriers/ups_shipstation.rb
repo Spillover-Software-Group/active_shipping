9371c1576b22c05746a4f63b02c56b8c8eda268b
Gustavo Camello <gucamello@gmail.com>
2024-08-20 16:58:03 +0100

    wip

--- lib/active_shipping/carriers/ups_shipstation.rb
+++ lib/active_shipping/carriers/ups_shipstation.rb
@@ -1,5 +1,11 @@
 module ActiveShipping
   class UPSShipstation < ::USPSShipstation
+    def requirements
+      [:api_key, :api_secret]
+    end
 
+    def find_rates(origin, destination, packages, options = {})
+      raise "GSUTAVO CAMELLO TEST".inspect
+    end
   end
 end
