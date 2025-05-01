bef14c0da6bd906954fad57f8f146549bb3692c3
Gustavo Camello <gucamello@gmail.com>
2025-05-01 12:01:20 +0100

    test

--- lib/active_shipping/carriers/usps_rest.rb
+++ lib/active_shipping/carriers/usps_rest.rb
@@ -69,7 +69,7 @@ module ActiveShipping
           )
 
           response = JSON.parse(request)
-
+          raise response.inspect
           package = {
             package: index,
             rates: generate_package_rates(response)
@@ -120,7 +120,6 @@ module ActiveShipping
     end
 
     def generate_package_rates(response)
-      raise response.inspect
       # USPS returns more than one from the same service
       # we find the minimun price for a service and return it
       services_rates = SERVICE_TYPES.map do |service_type|
