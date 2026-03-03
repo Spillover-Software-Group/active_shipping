0ba34e5059df6761a7895c6018f629c4d05adf37
Gustavo Camello <gucamello@gmail.com>
2026-03-03 11:19:10 +0000

    wip

--- lib/active_shipping/carriers/fedex_rest.rb
+++ lib/active_shipping/carriers/fedex_rest.rb
@@ -369,7 +369,11 @@ module ActiveShipping
       rate_reply_details.map do |detail|
         mail_class = detail["serviceType"]
         rated_shipment = detail["ratedShipmentDetails"]&.first
+        Rails.logger.info("[FedexRest] mail_class #{mail_class.inspect}")
+        Rails.logger.info("[FedexRest] rated_shipment: #{rated_shipment.inspect}")
+
         price = rated_shipment&.dig("totalNetFedExCharge")
+        Rails.logger.info("[FedexRest] price: #{price.inspect}")
 
         { mail_class: mail_class, price: price }
       end
